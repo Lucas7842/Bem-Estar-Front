be7d8de36f988c640911e1daa9dcb0284fb4dc25
Lucas Martins <lucasmartins1096@hotmail.com>
2024-05-09 17:37:52 -0300

    Criação da tela personal

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 
+import { NavItem } from 'react-bootstrap';
 import './App.css';
 //import { Team } from './Componentes/Telas/Academias/academia';
 //import ConsultaUsuario from './Componentes/ConsultaUsuario'; 
 //import  { HeaderComponent, Footer, HeroSection, AboutSection, ServicesSection, FeaturesSection } from './Componentes/Telas/Academias';
 //import { HeaderComponent, Breadcrumbs, Team, SearchSection } from './Componentes/Telas/Academias/academia';
-import {Header,NavBar,NavItem, Breadcrumbs,Teams, ContactForm,Footer} from "./Componentes/Telas/Academias/nutricao";
+//import {Header,NavBar,NavItem, Breadcrumbs,Teams, ContactForm,Footer} from "./Componentes/Telas/Academias/nutricao";
+import {Header,Breadcrumbs, NavBar} from "./Componentes/Telas/Academias/personal";
 
 function App() {
   return (
     <div className="App">
+      <NavItem/>
+      <NavBar/>
     <Header/>
-    <NavBar/>
-    <NavItem/>
     <Breadcrumbs/>
-    <Teams/>
-    <ContactForm/>
-    <Footer/>
     </div>
   );
 }
---
+++ src/Componentes/Telas/Academias/personal.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import"../Academias/styles.css";
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+const NavItem = ({ link, text }) => (
+  <li>
+    <a className="nav-link scrollto" href={link}>{text}</a>
+  </li>
+);
+
+// Componente para a barra de navegação
+const NavBar = () => (
+  <nav id="navbar" className="navbar order-last order-lg-0">
+    <ul>
+      <NavItem link="index.html" text="HOME" />
+      <NavItem link="academia.html" text="ACADEMIAS E PRAÇAS" />
+      {/* <NavItem link="dicas.html" text="DICAS E BEM-ESTAR" /> */}
+      <NavItem link="nutricao.html" text="NUTRICÃO" />
+      <NavItem link="personal.html" text="PERSONAL" />
+      <NavItem link="saudemental.html" text="SAÚDE MENTAL" />
+    </ul>
+    <i className="bi bi-list mobile-nav-toggle"></i>
+  </nav>
+);
+
+// Componente para o cabeçalho
+const Header = () => (
+  <header id="header" className="fixed-top header-inner-pages">
+    <div className="container d-flex align-items-center justify-content-lg-between">
+      <h1 className="logo me-auto me-lg-0">
+        <a href="index.html">PD<span>.</span></a>
+      </h1>
+      {/* <a href="index.html" className="logo me-auto me-lg-0">
+        <img src="assets/img/logo.png" alt="" className="img-fluid" />
+      </a> */}
+      <NavBar />
+      <a href="#about" className="get-started-btn scrollto">Cadastre seu Negocio</a>
+    </div>
+  </header>
+);
+
+const Breadcrumbs = () => {
+  return (
+    <section className="breadcrumbs">
+      <div className="container">
+        <div className="d-flex justify-content-between align-items-center">
+          <h2>Personal</h2>
+          <ol>
+            <li><a href="index.html">Home</a></li>
+            <li>Personal</li>
+          </ol>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export{NavItem,NavBar,Header, Breadcrumbs}
