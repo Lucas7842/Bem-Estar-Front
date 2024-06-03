34c5f8cf21278c84aa79cd7b5d1afcaba4a79791
MihhPaula <emilia.paula@fatec.sp.gov.br>
2024-06-03 19:24:48 -0300

    adicionado script de logout na tela de login

--- src/Componentes/Team/teamlogin.js
+++ src/Componentes/Team/teamlogin.js
@@ -44,40 +44,52 @@ function TeamLogin() {
     }
   };
 
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    navigate('/'); // Navega para a página inicial ou outra página após o logout
+  };
+
   return (
     <div className="cadastrar-usuario-container">
-      <h2>Login</h2>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label>Email:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={handleEmailChange}
-            className="form-control"
-            placeholder="Digite seu email"
-            style={{ padding: '12px' }}
-          />
-        </div>
-        <div className="form-group">
-          <label>Senha:</label>
-          <input
-            type="password"
-            value={senha}
-            onChange={handleSenhaChange}
-            className="form-control"
-            placeholder="Digite sua senha"
-            style={{ padding: '12px' }}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary" style={{ backgroundColor: '#ffc451', border: 'none' }}>Login</button>
-      </form>
-      <div>
-        {!isLoggedIn && (
-          <p className="mt-3 mb-0">Ainda não tem cadastro? <a href="/cadastrar-usuario" className="fw-bold" style={{ color: 'black' }}>Cadastre-se</a></p>
-        )}
-      </div>
+      {!isLoggedIn ? (
+        <>
+          <h2>Login</h2>
+          {error && <p style={{ color: 'red' }}>{error}</p>}
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label>Email:</label>
+              <input
+                type="email"
+                value={email}
+                onChange={handleEmailChange}
+                className="form-control"
+                placeholder="Digite seu email"
+                style={{ padding: '12px' }}
+              />
+            </div>
+            <div className="form-group">
+              <label>Senha:</label>
+              <input
+                type="password"
+                value={senha}
+                onChange={handleSenhaChange}
+                className="form-control"
+                placeholder="Digite sua senha"
+                style={{ padding: '12px' }}
+              />
+            </div>
+            <button type="submit" className="btn btn-primary" style={{ backgroundColor: '#ffc451', border: 'none' }}>Login</button>
+          </form>
+          <div>
+            <p className="mt-3 mb-0">Ainda não tem cadastro? <a href="/cadastrar-usuario" className="fw-bold" style={{ color: 'black' }}>Cadastre-se</a></p>
+          </div>
+        </>
+      ) : (
+        <>
+          <h2>Bem-vindo ao Portal Diadema ENforma</h2>
+          <button onClick={handleLogout} className="btn btn-primary" style={{ backgroundColor: '#ffc451', border: 'none' }}>Logout</button>
+        </>
+      )}
     </div>
   );
 }
